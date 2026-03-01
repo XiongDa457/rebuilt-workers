9d12081807241056817ce285f3e2e6599bf399ad
Dalton Su <dalton.jin.su@gmail.com>
2026-03-01 18:50:53 -0500

    set up D1 database

---
+++ schema.sql
@@ -0,0 +1,59 @@
+DROP TABLE IF EXISTS Announcements;
+CREATE TABLE IF NOT EXISTS Announcements (
+  ID TEXT PRIMARY KEY,
+  Time INTEGER,
+  Message TEXT
+);
+
+DROP TABLE IF EXISTS PartsRequests;
+CREATE TABLE IF NOT EXISTS PartsRequests (
+  ReqID TEXT PRIMARY KEY,
+  Time INTEGER,
+  Team INTEGER,
+  Parts TEXT
+);
+
+DROP TABLE IF EXISTS Teams;
+CREATE TABLE IF NOT EXISTS Teams (
+  TeamNumber INTEGER PRIMARY KEY,
+  PitsData TEXT
+);
+
+DROP TABLE IF EXISTS Scouters;
+CREATE TABLE IF NOT EXISTS Scouters (
+  StudentNumber INTEGER PRIMARY KEY,
+  Name TEXT NOT NULL,
+  Token TEXT
+);
+
+DROP TABLE IF EXISTS Matches;
+CREATE TABLE IF NOT EXISTS Matches (
+  MatchID TEXT PRIMARY KEY,
+  Times TEXT
+);
+
+DROP TABLE IF EXISTS TeamToMatch;
+CREATE TABLE IF NOT EXISTS TeamToMatch (
+  TeamNumber INTEGER NOT NULL,
+  MatchID TEXT NOT NULL,
+  Alliance TEXT NOT NULL,
+  TeamIndex INTEGER NOT NULL,
+  MatchData TEXT,
+  PRIMARY KEY (TeamNumber, MatchID),
+  FOREIGN KEY (TeamNumber) REFERENCES Teams(TeamNumber),
+  FOREIGN KEY (MatchID) REFERENCES Matches(MatchID)
+);
+
+DROP TABLE IF EXISTS ScouterToMatch;
+CREATE TABLE IF NOT EXISTS ScouterToMatch (
+  StudentNumber INTEGER NOT NULL,
+  MatchID TEXT NOT NULL,
+  Alliance TEXT,
+  TeamIndex INTEGER,
+  PRIMARY KEY (StudentNumber, MatchID),
+  FOREIGN KEY (StudentNumber) REFERENCES Scouters(StudentNumber),
+  FOREIGN KEY (MatchID) REFERENCES Matches(MatchID)
+);
+
+
+
